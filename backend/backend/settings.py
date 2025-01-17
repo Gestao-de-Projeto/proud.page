abf617fa2c3cf12dc5cd6b3d3c2024b4103cfd3e
LexJJ6 <joaoalexcp1717@gmail.com>
2025-01-17 20:52:43 +0000

    dj database url corrigido

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,9 +78,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(default=os.getenv('DATABASE_URL'))
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
